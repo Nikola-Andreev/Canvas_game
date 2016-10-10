6661f9271ad04c4af90e384bd76503649da5e769
Nikola Andreev <noka@abv.bg>
2016-10-10 20:13:40 +0300

    Make moving background

--- game.js
+++ game.js
@@ -82,3 +82,88 @@ canvas {
 </style>
 </head>
 <body onload="init()">
+    <!-- The canvas for the panning background -->
+<canvas id="background" width="640" height="610"></canvas>
+
+    <div id="hero"></div>
+    <div id="rocket"></div>
+    <div id="score"></div>
+    <div id="gameover">GAME OVER</div>
+<div id="health"></div>
+    <button id="refresh" onclick="playAgain()">Play Again</button>
+
+<script>
+
+let game = new Game()
+let gameIsOver = false
+function init() {
+    if(game.init())
+        game.start()
+}
+
+let imageRepository = new function() {
+    // Define images
+    this.empty = null
+    this.background = new Image()
+
+    // Set images src
+    this.background.src = "space2.png"
+}
+function Drawable() {
+    this.init = function(x, y) {
+        // Defualt variables
+        this.x = x
+        this.y = y
+    }
+
+    this.speed = 0
+    this.canvasWidth = 0
+    this.canvasHeight = 0
+
+    // Define abstract function to be implemented in child objects
+    this.draw = function() {
+    }
+}
+
+function Background() {
+    this.speed = 1 // Redefine speed of the background for panning
+
+    // Implement abstract function
+    this.draw = function() {
+        // Pan background
+        this.y += this.speed
+        this.context.drawImage(imageRepository.background, this.x +140, this.y)
+        // Draw another image at the top edge of the first image
+        this.context.drawImage(imageRepository.background, this.x, this.y - this.canvasHeight -60)
+        // If the image scrolled off the screen, reset
+        if (this.y >= this.canvasHeight)
+            this.y = 0
+    };
+}
+// Set Background to inherit properties from Drawable
+Background.prototype = new Drawable()
+
+function Game() {
+    this.init = function() {
+        // Get the canvas element
+        this.bgCanvas = document.getElementById('background');
+        // Test to see if canvas is supported
+        if (this.bgCanvas.getContext) {
+            this.bgContext = this.bgCanvas.getContext('2d')
+            // Initialize objects to contain their context and canvas
+            Background.prototype.context = this.bgContext;
+            Background.prototype.canvasWidth = this.bgCanvas.width;
+            Background.prototype.canvasHeight = this.bgCanvas.height;
+            // Initialize the background object
+            this.background = new Background()
+            this.background.init(0,0) // Set draw point to 0,0
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // Start the animation loop
+    this.start = function() {
+        animate();
+    }
+}
